cf852b4f3e53cdbdf6a73e34a6dd643f096076f8
IgorShishi20 <70200537+IgorShishi20@users.noreply.github.com>
2020-10-21 22:23:44 +0300

    Add files via upload

---
+++ week10/ex2.sh
@@ -0,0 +1,4 @@
+touch ./week01/file.txt
+link ./week01/file.txt ./week10/_ex2.txt
+ls -i ./week01/file.txt>>ex2.txt
+
---
+++ week10/ex2_2.sh
@@ -0,0 +1,2 @@
+find .  -inum 26740122787721640>>ex2.txt
+find . -inum 26740122787721640 -exec rm {} \;
